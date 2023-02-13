36a28e2bd5ae9332c07ac5b3992014888b86874e
chengqinglin <chengqinglin@icloud.com>
2023-02-13 17:52:06 +0800

    draft
    Signed-off-by: chengqinglin <chengqinglin@icloud.com>

--- src/diff/difft.rs
+++ src/diff/difft.rs
@@ -3,10 +3,15 @@ use crate::diff::Diff;
 use crate::diff::Process;
 use crate::kube::dynamic_object;
 use crate::persistent;
-use difft_lib::{diff_file, options, print_diff_result};
+
+use difft_lib::{diff_file, options, print_diff_result, tui_diff_result, FgColor};
 use kube::api::DynamicObject;
 use std::path::PathBuf;
-use tui::widgets::Paragraph;
+use tui::{
+    style::{Color, Style},
+    text::{Span, Spans},
+    widgets::{Paragraph, Wrap},
+};
 
 pub struct Difft {
     include_managed_fields: bool,
@@ -69,11 +74,58 @@ impl<'a> Diff<'a> for Difft {
 
         // init delta args
         let (minus_file, plus_file) = persistent::tmp_store(&l, &r);
-        // TODO: difft diff here
 
-        (
-            Paragraph::new("left").alignment(tui::layout::Alignment::Left),
-            Paragraph::new("right").alignment(tui::layout::Alignment::Left),
-        )
+        let graph_limit = options::DEFAULT_GRAPH_LIMIT;
+        let byte_limit = options::DEFAULT_BYTE_LIMIT;
+        let display_options = options::DisplayOptions {
+            background_color: options::BackgroundColor::Dark,
+            use_color: true,
+            print_unchanged: false,
+            tab_width: options::DEFAULT_TAB_WIDTH,
+            display_mode: options::DisplayMode::SideBySide,
+            display_width: options::detect_display_width(),
+            syntax_highlight: true,
+            in_vcs: true,
+        };
+        let language_override = None;
+        let missing_as_empty = false;
+        let diff_result = diff_file(
+            minus_file.to_str().unwrap(),
+            plus_file.to_str().unwrap(),
+            minus_file.as_path(),
+            plus_file.as_path(),
+            &display_options,
+            missing_as_empty,
+            graph_limit,
+            byte_limit,
+            language_override,
+        );
+
+        let (l_res, r_res) = tui_diff_result(&display_options, &diff_result);
+        (to_paragraph(l_res), to_paragraph(r_res))
     }
+}
+
+fn to_paragraph<'a>(result: Vec<Vec<(String, FgColor)>>) -> Paragraph<'a> {
+    Paragraph::new(
+        result
+            .iter()
+            .map(|line| {
+                let spans = line
+                    .iter()
+                    .map(|span| {
+                        let (content, color) = span;
+                        let c = match color {
+                            FgColor::White => Color::White,
+                            FgColor::Red => Color::LightRed,
+                            FgColor::Green => Color::LightGreen,
+                        };
+                        Span::styled(content.clone(), Style::default().fg(c))
+                    })
+                    .collect::<Vec<_>>();
+                Spans::from(spans)
+            })
+            .collect::<Vec<_>>(),
+    )
+    .wrap(Wrap { trim: true })
 }
